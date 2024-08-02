737b520ea76b306146e606d413be0e71e28508c6
Daniel La Rocque <dlarocque@google.com>
2024-08-02 16:32:28 -0400

    Create reproduction app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  window.addEventListener('storage', (event) => {
+    console.log(`MAIN storage event: ${JSON.stringify(event)}`);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        title="My Iframe"
+        src='http://localhost:3001/index.html'
+        width="600"
+        height="400"
+      />
     </div>
   );
 }
